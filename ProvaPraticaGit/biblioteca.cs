b445f8804ce51cdd5153441045370d90e0cb9414
Alessandro Tome <ALESSANDRO.TOME@local.ittcesena.it>
2022-11-17 08:49:19 +0100

    Versione 3 con branch Biblioteca

---
+++ ProvaPraticaGit/biblioteca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaPraticaGit
+{
+    internal class biblioteca
+    {
+        internal string nome { get; set; }
+        internal string indirizzo { get; set; }
+        internal string orariApertura { get; set; }
+        internal string orariChiusura { get; set; }
+        internal int elencoLibri { get; set; }
+
+        public void Biblioteca (string nome, string indirizzo, string orariApertura, string orariChiusura, int elencoLibri)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.orariApertura = orariApertura;
+            this.orariChiusura = orariChiusura;
+            this.elencoLibri = elencoLibri;
+        }
+    }
+}
