592781f5763d3df4bc1846381e3f153326fe101f
Alessandro Tome <ALESSANDRO.TOME@local.ittcesena.it>
2022-11-17 08:31:41 +0100

    Versione 1 con branch Libro

--- ProvaPraticaGit/libro.cs
+++ ProvaPraticaGit/libro.cs
@@ -8,5 +8,44 @@ namespace ProvaPraticaGit
 {
     internal class libro
     {
+        internal string autore { get; set; }
+        internal string titolo { get; set; }
+        internal string annoDiPubblicazione { get; set; }
+        internal string editore { get; set; }
+        internal int numeroDiPagine { get; set; }
+
+        public string toString (string Autore, string Titolo, string AnnoDiPubblicazione, string Editore, int NumeroDiPagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            AnnoDiPubblicazione = annoDiPubblicazione;
+            Editore = editore;
+            NumeroDiPagine = numeroDiPagine;
+
+            string datiLibro = "";
+            datiLibro = Autore + Titolo + AnnoDiPubblicazione + Editore + NumeroDiPagine;
+            return datiLibro;
+        }
+
+        public void readingTime(int NumeroPagine)
+        {
+            NumeroPagine = numeroDiPagine;
+
+            if(NumeroPagine < 100)
+            {
+                Console.WriteLine("Il tempo di lettura è di un'ora.");
+            }
+            else
+            {
+                if(NumeroPagine > 100 && NumeroPagine < 200)
+                {
+                    Console.WriteLine("Il tempo di lettura è pari a 2 ore.");
+                }
+                else
+                {
+                    Console.WriteLine("Il tempo di lettura è superiore a 2 ore.");
+                }
+            }
+        }
     }
 }
